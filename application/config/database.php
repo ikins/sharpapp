59b7b6347067dcb1068de1401f5d53259c3d0fb2
ikins <ikin.musyafa@gmail.com>
2021-02-19 00:54:11 +0700

    cms sharp

--- application/config/database.php
+++ application/config/database.php
@@ -6,7 +6,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost:',
 	'username' => '',
 	'password' => '',
 	'database' => '',
